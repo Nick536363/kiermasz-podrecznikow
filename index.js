d3c44aa39a3b9a21bc9c2202b91938b6b8e97656
Nick <3383371nik@gmail.com>
2026-09-13 15:55:53 +0200

    Dodano logi i potwierdzenie sprzedaży

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const session = require("express-session")
 const crypto = require("node:crypto")
+const fs = require("fs")
+const format = require("node.date-time")
 const { DatabaseSync } = require("node:sqlite")
 require("dotenv").config()
 const database = new DatabaseSync(__dirname+"/"+process.env.DB_NAME)
 const app = express()
 const PORT = process.env.PORT
+const COMMISION_PERCENT = 0.2
+
+function log(file, message){
+    let date = new Date().format("Y-MM-dd HH:mm:SS")
+    fs.appendFile(file, `${date} ${message}\n`, (err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+    
+}
 
 app.use(session({
     secret: process.env.SECRET,
@@ -36,7 +49,18 @@ const all_payments_prepare = database.prepare("SELECT SUM(pupil_price) AS money 
 const all_paymements_commision_preapre = database.prepare("SELECT SUM(commision) AS commision FROM books WHERE payment_method=? AND status = 'Sprzedana'")
 const books_by_payment_prepare = database.prepare("SELECT * FROM books WHERE payment_method LIKE ?")
 const sell_book_prepare = database.prepare("UPDATE books SET status = 'Sprzedana', payment_method = ? WHERE ID = ? AND status = 'Nie sprzedana'")
+const user_by_id_prepare = database.prepare("SELECT username FROM users WHERE uID = ?")
 
+
+app.use((req, res, next)=>{
+    if(!req.session.user){
+        log("log.log", `${req.ip} wszedł na ${req.url} (${req.method})`)
+    }
+    else{
+        log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} wszedł na ${req.url} (${req.method})`)
+    }
+    next()
+})
 
 app.get("/", (req, res) => {
     if(!req.session.user){
@@ -111,12 +135,10 @@ app.post("/book/add", (req, res)=>{
     if(!req.session.user){
         return res.redirect("/login")
     }
-    let currentDate = new Date()
-    let day = currentDate.getDate()
-    let month = currentDate.getMonth()
-    let year = currentDate.getFullYear()
-    let stringDate = day+"-"+month+"-"+year
-    new_book_prepare.run(req.body.title, req.body.pupil, parseInt(req.body.pupil_price), Math.floor(parseInt(req.body.pupil_price)/10), "Nie sprzedana", stringDate)
+    let date = new Date().format("Y-MM-DD")
+    if(parseInt(req.body.pupil_price) < 10)
+    new_book_prepare.run(req.body.title, req.body.pupil, parseInt(req.body.pupil_price), Math.floor(parseInt(req.body.pupil_price)*COMMISION_PERCENT), "Nie sprzedana", date)
+    log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} dodał nową książkę`)
     return res.redirect("/")
 })
 
@@ -165,6 +187,7 @@ app.post("/", (req, res)=>{
         books = books_by_payment_prepare.all(`%${req.body.searchfield}%`)
     }
 
+    log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} wyszukał ${req.body.searchfield} z kategorii ${req.body.searchOn}`)
 
     res.render(__dirname+"/templates/index.ejs", {
         books: books,
@@ -183,8 +206,30 @@ app.post("/book/change", (req, res)=>{
         })
     }
     else if(req.body.ID){
-        update_book_preapre.run(req.body.title, req.body.pupil, parseInt(req.body.pupil_price), Math.floor(parseInt(req.body.pupil_price)/10), req.body.status, req.body.payment_method, parseInt(req.body.ID))
+        update_book_preapre.run(req.body.title, req.body.pupil, parseInt(req.body.pupil_price), Math.floor(parseInt(req.body.pupil_price)*COMMISION_PERCENT), req.body.status, req.body.payment_method, parseInt(req.body.ID))
+        log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} zmienił książkę ${req.body.ID}`)
         return res.redirect("/")
+    }
+})
+
+app.get("/book/sell/confirm", (req, res)=>{
+    if(!req.session.user){
+        return res.redirect("/login")
+    }
+    if(req.query.idC){
+        res.render(__dirname+"/templates/confirm.ejs", {
+            book: book_by_id_prepare.get(req.query.idC),
+            payment_method: "gotówkę",
+            query: req.query
+        })
+    }
+
+    else if(req.query.idB){
+        res.render(__dirname+"/templates/confirm.ejs", {
+            book: book_by_id_prepare.get(req.query.idB),
+            payment_method: "BLIK",
+            query: req.query
+        })
     }
 })
 
@@ -194,13 +239,17 @@ app.post("/book/sell", (req, res)=>{
     }
     if(req.body.idC){
         if(sell_book_prepare.run("Gotówka", req.body.idC).changes != 1){
+            log("warning.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} spróbował sprzedać książkę ${req.body.idC} za gotówkę, błąd sprzedaży`)
             return res.redirect("/book/sell/error")
         }
+        log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} sprzedał książkę ${req.body.idC} za gotówkę`)
     }
     else if(req.body.idB){
         if(sell_book_prepare.run("BLIK", req.body.idB).changes != 1){
+            log("warning.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} spróbował sprzedać książkę ${req.body.idB} za BLIK, błąd sprzedaży`)
             return res.redirect("/book/sell/error")
         }
+        log("log.log", `User ${user_by_id_prepare.get(req.session.user).username} z ${req.ip} sprzedał książkę ${req.body.idB} za BLIK`)
     }
     return res.redirect("/")
 })
